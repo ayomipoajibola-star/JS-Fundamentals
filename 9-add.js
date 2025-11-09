7b07dc12cf2293bc33accc82d6cd003df25240c1
Ayomipo Ajibola <ayomipo.ajibola@gmail.com>
2025-11-09 07:32:22 -0800

    Add

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+function add(a, b) {
+  return a + b;
+}
+
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+console.log(add(a, b));
